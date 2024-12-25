2f007d5685465283c3a9a5128ec9f9f17d3019c3
gojo-satorou-v7 <118048127+gojo-satorou-v7@users.noreply.github.com>
2024-12-26 01:39:27 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,10 @@
-
-
- var onfetch=e=>{
-    console.log(e.request.url);
-    if(e.request.url=="https://gojo-satorou-v7.github.io/response.redirect/data"){
-        console.log("match");
-      e.respondWith(fetch("/").then(()=>{
-          return Response.redirect("data://text/html,<script>alert(document.domain)</script>");
-      }));
-    }
-  }
+var onfetch=e=>{
+   console.log(e.request.url);
+   if(e.request.url=="https://gojo-satorou-v7.github.io/response.redirect/data"){
+       console.log("match");
+     e.respondWith(fetch("/").then(()=>{
+         return Response.redirect("data://text/html,<script>alert(document.domain)</script>");
+     }));
+   }
+ }
   
