c8a968c44023c4d4f467f2c49928a022a730a8f5
gojo-satorou-v7 <118048127+gojo-satorou-v7@users.noreply.github.com>
2024-12-26 01:38:56 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 
  var onfetch=e=>{
     console.log(e.request.url);
-    if(e.request.url=="/data"){
+    if(e.request.url=="https://gojo-satorou-v7.github.io/response.redirect/data"){
         console.log("match");
       e.respondWith(fetch("/").then(()=>{
           return Response.redirect("data://text/html,<script>alert(document.domain)</script>");
